ee7a6f0c5d4216400719ee606eb3e921563a963f
Даниил Шубников <@izhtc.net>
2025-02-03 14:29:51 +0400

    добавил пинг сервера и порт для билда

--- main.js
+++ main.js
@@ -2,15 +2,49 @@ require('dotenv').config()
 const { VK, Keyboard } = require('vk-io');
 const { HearManager } = require('@vk-io/hear');
 
+const express = require('express')
+const axios = require('axios');
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Что ты тут забыл? XD',)
+})
+
+app.listen(process.env.PORT)
+
+// URL вашего сервера
+const url = process.env.PROD_URL;
+
+// Интервал пингования в миллисекундах (например, 1 минут)
+const pingInterval = 13 * 60 * 1000;
+// const pingInterval = 10 * 1000;
+
+// Функция для пингования сервера 
+function pingServer() {
+    axios.get(url)
+        .then(() => {
+            const now = new Date();
+            const offset = now.getTimezoneOffset() / 60; // Получаем смещение часового пояса в часах
+            const gmtPlusFour = now.getTime() + (offset + 4) * 60 * 60 * 1000; // Добавляем смещение часового пояса к времени
+            const pingTime = new Date(gmtPlusFour).toLocaleString(); // Преобразуем время в локальную строку даты и времени
+            console.log(`Pinged server at ${pingTime}`);
+        })
+        .catch((err) => {
+            console.error(`Error pinging server: ${err.message}`);
+        });
+}
+
+// Пингуем сервер каждые pingInterval миллисекунд
+setInterval(pingServer, pingInterval);
 const vk = new VK({
-    token: process.env.TOKEN
+    token: process.env.TOKEN 
 });
 
 const hearManager = new HearManager();
 
 vk.updates.on('message_new', (context, next) => {
     const { messagePayload } = context;
-    
+
     context.state.command = messagePayload && messagePayload.command
         ? messagePayload.command
         : null;
